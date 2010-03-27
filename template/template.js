d4fe3d39ca096ba00ef573fdf9c8e7a68fb623d7
Dmitry Fitiskin <d.fitiskin@gmail.com>
2010-03-28 00:38:10 +0400

    Добавлена возможность подключать внешние шаблоны как <script src="%path%">

--- template/template.js
+++ template/template.js
@@ -22,15 +22,74 @@ function Template(id)
 	var tpl;
 	if (tpl = document.getElementById(id))
 	{
-		this.content = tpl.innerHTML;
+		if (tpl.src)
+		{
+			this.load(tpl.src);
+		}
+		else
+		{
+            this.content = tpl.innerHTML;
+		}
 	}
 }
 
+Template.prototype.loaded = true;
 Template.prototype.content = new String;
+
+Template.prototype.load = function(src)
+{
+	this.loaded = false;
+	var http = this.createRequestObject();
+    if (http)   
+    {  
+        http.open('get', src);
+        var target = this;
+        http.onreadystatechange = function ()   
+        {  
+            if(http.readyState == 4)   
+            {  
+                target.content = http.responseText;
+                target.loaded = true;
+                
+                if (target.renderAfterLoad)
+                {
+                	target.renderInto(target.renderAfterLoad['data'], target.renderAfterLoad['target']);
+                }
+            }  
+        }  
+        http.send(null);      
+    }
+}
+
+Template.prototype.createRequestObject = function()   
+{  
+    try 
+	{ 
+        return new XMLHttpRequest();
+    }  
+    catch(e)   
+    {  
+        try 
+    	{ 
+            return new ActiveXObject('Msxml2.XMLHTTP'); 
+        }  
+        catch(e)   
+        {  
+            try 
+        	{ 
+                return new ActiveXObject('Microsoft.XMLHTTP'); 
+            }  
+            catch(e)
+        	{ 
+                return null;
+            }  
+        }  
+    }  
+}  
 
 Template.prototype.render = function(data)
 {
-	return this.content.supplant(data);
+    return this.content.supplant(data);
 }
 
 Template.prototype.renderInto = function(data, target)
@@ -38,6 +97,17 @@ Template.prototype.renderInto = function(data, target)
 	var node;
 	if (node = document.getElementById(target))
 	{
-		node.innerHTML = this.content.supplant(data); 
+        if (this.loaded)
+        {
+        	node.innerHTML = this.render(data);
+        	this.renderAfterLoad = null;
+        }
+        else
+        {
+        	this.renderAfterLoad = {
+        		data: data,
+        		target: target
+        	}
+        }
 	}
 }
